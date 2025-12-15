6254a80bd38d16ac7038caf89216cd95240587f5
Rene31416 <renemirandau4@gmail.com>
2025-12-14 20:43:05 -0600

    feat(stack): deploy packaged Python lambda

--- lib/py_agent-stack.ts
+++ lib/py_agent-stack.ts
@@ -1,11 +1,18 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib/core";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import path from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PyAgentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const lambda = new Function(this, "python-lambda", {
+      runtime: Runtime.PYTHON_3_12,
+      handler: "agentLambda.agent.handler",
+      code: Code.fromAsset("src/lambdas/lambda_package.zip"),
+    });
     // The code that defines your stack goes here
 
     // example resource
